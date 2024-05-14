56ab3495a2da4d7cbbf964d787f835b86d7cb6d5
nakedtatsuya <t.fukui@prog-8.com>
2024-05-14 20:45:42 +0900

    fix condition

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-import { AnthropicBedrock } from '@anthropic-ai/bedrock-sdk';
+const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-bedrock-runtime");
 
-// Note: this assumes you have configured AWS credentials in a way
-// that the AWS Node SDK will recognise, typicaly a shared `~/.aws/credentials`
-// file or `AWS_ACCESS_KEY_ID` & `AWS_SECRET_ACCESS_KEY` environment variables.
-//
-// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/setting-credentials-node.html
-const anthropic = new AnthropicBedrock();
+const client = new BedrockRuntimeClient({
+  region: "us-east-1",
+});
 
-async function main() {
-  const message = await anthropic.messages.create({
-    model: 'anthropic.claude-3-sonnet-20240229-v1:0',
+const params = {
+  modelId: "anthropic.claude-3-sonnet-20240229-v1:0",
+  contentType: "application/json",
+  accept: "application/json",
+  body: JSON.stringify({
+    anthropic_version: "bedrock-2023-05-31",
+    max_tokens: 1000,
     messages: [
       {
-        role: 'user',
-        content: 'Hello! あなたのモデルはどのリージョンのBedrockで動作していますか？',
-      },
-    ],
-    max_tokens: 1024,
-  });
-  console.log(message);
-}
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "こんにちは、"
+          }
+        ]
+      }
+    ]
+  }),
+};
 
-main();
+const invokeModel = async () => {
+  try {
+    const command = new InvokeModelCommand(params);
+    const response = await client.send(command);
+    // バイナリデータをテキストに変換
+    const textDecoder = new TextDecoder("utf-8");
+    const responseBodyText = textDecoder.decode(response.body);
+
+    console.log("Response Text:", responseBodyText);
+  } catch (error) {
+    console.error("Error invoking model:", error);
+  }
+};
+
+invokeModel();
